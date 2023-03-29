0a793140382207896df643679020b774b4281bb3
“Alfonso <alfonso.domenici@gmail.com>
2023-03-29 15:11:29 +0200

    Nuovi esempi...

---
+++ 32-libreria-rest-st/src/test/java/NewEmptyJUnitTest.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author ospite
+ */
+public class NewEmptyJUnitTest {
+
+    public NewEmptyJUnitTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void hello() {
+        assertTrue(true);
+    }
+}
